93b4da1dbaa71216f73bdd5700b6324636c42f99
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-06 16:02:27 +0100

    chore: use nvm

--- init-vm.sh
+++ init-vm.sh
@@ -6,19 +6,16 @@ cd setup-vm
 # update apt-get
 sudo apt-get update
 
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-
-sudo apt-get update
-
 # install node, npm, dig & jq
-sudo apt-get install -y dnsutils jq node npm
+sudo apt-get install -y dnsutils jq
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm install --lts
 
 # install docker
 #Add Docker's official GPG key:
+# sudo apt-get install -y ca-certificates curl gnupg
 # sudo install -m 0755 -d /etc/apt/keyrings
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 # sudo chmod a+r /etc/apt/keyrings/docker.gpg
