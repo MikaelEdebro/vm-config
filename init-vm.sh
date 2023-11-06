bdb970a8712c2644acb12e8a155f72133417d723
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-06 20:33:44 +0100

    chore: modify script

--- init-vm.sh
+++ init-vm.sh
@@ -14,23 +14,23 @@ curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
 
 # install docker
 # Add Docker's official GPG key:
-# sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt update
-# apt-cache policy docker-ce
-# sudo apt-get install -y docker-ce
-# # run docker command without sudo
-# sudo usermod -aG docker ${USER}
-# su - ${USER}
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+# run docker command without sudo
+sudo usermod -aG docker ${USER}
+sudo su - ${USER}
 
 
-# # install powershell
+# install powershell
 mkdir powershell
 cd powershell
 wget https://github.com/PowerShell/PowerShell/releases/download/v7.3.9/powershell-7.3.9-linux-x64.tar.gz
 tar -xvf powershell-7.3.9-linux-x64.tar.gz -C .
-sudo ln -s ~/setup-vm/powershell/pwsh /usr/local/bin/pwsh
+sudo ln -fs ~/setup-vm/powershell/pwsh /usr/local/bin/pwsh
 
 # install azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
