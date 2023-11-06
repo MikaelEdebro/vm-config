72d71e5ae26417ba67c557c513439821d36b839f
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-06 22:18:07 +0100

    chore: cleanup

--- init-vm.sh
+++ init-vm.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -e
 
-mkdir -p setup-vm
-cd setup-vm
-
 # update apt-get
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 
 # install dig & jq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq dnsutils jq
 
+# needed for dpkg to work
 source ~/.bashrc
 
-# install node & npm using silent mode for curl and n
+# install node & npm using https://github.com/tj/n
 curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
 
 # Install docker and other prerequisites
@@ -26,35 +24,10 @@ sudo usermod -aG docker azureuser
 # Install PowerShell
 mkdir -p powershell
 cd powershell
-# wget -q https://github.com/PowerShell/PowerShell/releases/download/v7.3.9/powershell-7.3.9-linux-x64.tar.gz
-# tar -xzf powershell-7.3.9-linux-x64.tar.gz
-
-# # Check if 'pwsh' exists and link it to /usr/local/bin
-# if [ -f "$PWD/pwsh" ]; then
-#     # Set execute permissions
-#     chmod +x $PWD/pwsh
-#     # Link 'pwsh' to /usr/local/bin
-#     sudo ln -fs $PWD/pwsh /usr/local/bin/pwsh
-#     echo "PowerShell has been installed and linked."
-# else
-#     echo "Error: PowerShell binary 'pwsh' does not exist in the extracted directory."
-#     exit 1
-# fi
-# Download PowerShell .deb package
 wget -q "https://github.com/PowerShell/PowerShell/releases/download/v7.3.9/powershell_7.3.9-1.deb_amd64.deb"
-
-# Install the downloaded package
 sudo dpkg -i powershell_7.3.9-1.deb_amd64.deb
-
-# Check for missing dependencies and install them
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -yf
-
-# Verify the installation
 sudo ln -s /usr/bin/pwsh /usr/local/bin/pwsh
-
-pwsh -v
-
 
 # Install Azure CLI
 curl -sSL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
