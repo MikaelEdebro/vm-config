7475a3195aa47331b408314f8653961b4b601576
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-06 17:02:54 +0100

    chore: use apt-get

--- init-vm.sh
+++ init-vm.sh
@@ -14,12 +14,12 @@ curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
 
 # install docker
 # Add Docker's official GPG key:
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install -y docker-ce
+sudo apt-get install -y docker-ce
 # run docker command without sudo
 sudo usermod -aG docker ${USER}
 su - ${USER}
