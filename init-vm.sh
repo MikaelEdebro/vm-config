98b8073d88506376322053335b7cff6dcacc1d9b
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-06 20:39:39 +0100

    chore: add quiet flag

--- init-vm.sh
+++ init-vm.sh
@@ -7,19 +7,19 @@ cd setup-vm
 sudo apt-get update
 
 # install dig & jq
-sudo apt-get install -y dnsutils jq
+sudo apt-get install -yq dnsutils jq
 
 # install node & npm
 curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
 
 # install docker
 # Add Docker's official GPG key:
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -yq apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
+sudo apt-get install -yq docker-ce
 # run docker command without sudo
 sudo usermod -aG docker ${USER}
 sudo su - ${USER}
