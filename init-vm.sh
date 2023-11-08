70dc4715ff6076f8dcb9a340218326e5299614ed
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-08 16:01:41 +0100

    fix: download vsu

--- init-vm.sh
+++ init-vm.sh
@@ -31,3 +31,8 @@ sudo ln -s /usr/bin/pwsh /usr/local/bin/pwsh
 
 # Install Azure CLI
 curl -sSL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+source ~/.bashrc
+
+az login --identity --username /subscriptions/50a73d67-b395-4eef-b655-9cd55a7fbbf3/resourcegroups/rg-vce-pipeline-dev/providers/Microsoft.ManagedIdentity/userAssignedIdentities/sp-vce-pipeline-dev
+az storage blob download --account-name savcepipelinedev --container-name vsu-cli --name vsu-cli-latest.zip --file ~/vsu-cli/vsu-cli-latest.zip --auth-mode login
