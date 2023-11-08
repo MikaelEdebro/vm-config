a9dbea1d7d41d0a2875003d0aee54e8cec7bebc0
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2023-11-08 20:48:33 +0100

    vsu install

--- init-vm.sh
+++ init-vm.sh
@@ -42,6 +42,8 @@ az login --identity --username /subscriptions/50a73d67-b395-4eef-b655-9cd55a7fbb
 az storage blob download --account-name savcepipelinedev --container-name vsu-cli --name vsu-cli-latest.zip --file ./vsu-cli-latest.zip --auth-mode login
 unzip -q ./vsu-cli-latest.zip -d .
 npm pkg delete scripts.prepare
-sudo npm install -g
+npm install --omit=dev
+cd ..
+sudo npm install -g ./vsu-cli --omit=dev
 source ~/.bashrc
 vsu --help
