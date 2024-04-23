4579721a60e21c687f7bb46c941d698426d88ace
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2024-04-23 14:17:46 +0200

    don't use declare

--- init-vm.sh
+++ init-vm.sh
@@ -38,20 +38,18 @@ curl -sSL https://aka.ms/InstallAzureCLIDeb | sudo bash
 # az account set --subscription d2e4cd6f-ef6e-476a-a6d7-ef1965d9f557
 az login --identity --username /subscriptions/d2e4cd6f-ef6e-476a-a6d7-ef1965d9f557/resourcegroups/rg-vce-devops-agents-prd/providers/Microsoft.ManagedIdentity/userAssignedIdentities/sp-vce-devops-agents
 patToken=$(az keyvault secret show --vault-name kv-vce-devops-agents-prd --name AzDevopsPatToken --query "value" --output tsv)
+echo "patToken: ${patToken}"
 
-declare -R NPM_FEED_URL="pkgs.dev.azure.com/VolvoGroup-MASDCL/VCEBusInfoServLayer/_packaging/VCE-MS-PoC/npm"
-
-declare PAT_BASE64
-
-PAT_BASE64=$(print "${patToken:?}" | base64 -w 0)
+NPM_FEED_URL="pkgs.dev.azure.com/VolvoGroup-MASDCL/VCEBusInfoServLayer/_packaging/VCE-MS-PoC/npm"
+PAT_BASE64=$(echo "${patToken}" | base64 -w 0)
 
 # token as is
-npm config --user set "//${NPM_FEED_URL:?}/registry/:username" "VolvoGroup-MASDCL"
-npm config --user set "//${NPM_FEED_URL:?}/registry/:_password" "${PAT_BASE64:?}"
-npm config --user set "//${NPM_FEED_URL:?}/registry/:email" "npm requires email to be set but doesn't use the value"
-npm config --user set registry "https://${NPM_FEED_URL:?}/registry"
+npm config --user set "//${NPM_FEED_URL}/registry/:username" "VolvoGroup-MASDCL"
+npm config --user set "//${NPM_FEED_URL}/registry/:_password" "${PAT_BASE64}"
+npm config --user set "//${NPM_FEED_URL}/registry/:email" "npm requires email to be set but doesn't use the value"
+npm config --user set registry "https://${NPM_FEED_URL}/registry"
 
 npm i @volvo/vce-service-util@latest -g
 
 # pull base images to speed up docker build
-docker pull node:lts-alpine
+# docker pull node:lts-alpine
