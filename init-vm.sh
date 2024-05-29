16c57c9a73931a14099bdf6eb81296e531f63847
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2024-05-29 20:40:49 +0200

    refactor: speed up init script

--- init-vm.sh
+++ init-vm.sh
@@ -30,16 +30,11 @@ if [[ "$(whoami)" != "AzDevOps" ]]; then
   exit 0
 fi
 
-# update apt-get
-sudo apt-get update
-
 # install dig, jq, and other utils
-sudo apt-get install -yq dnsutils jq zip unzip 
+sudo apt-get install -yq curl ca-certificates dnsutils jq zip wget unzip postgresql-client python3-pip
 
 # install docker and other prerequisites (https://docs.docker.com/engine/install/ubuntu/)
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -55,6 +50,21 @@ sudo apt-get update
 sudo apt-get install -yq docker-ce
 sudo usermod -a -G docker AzDevOps
 
+# install node lts
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&
+  sudo apt-get install -y nodejs
+
+# install PowerShell
+sudo apt-get install -y apt-transport-https software-properties-common
+
+# shellcheck disable=SC1091
+source /etc/os-release
+wget -q https://packages.microsoft.com/config/ubuntu/"${VERSION_ID:?}"/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+
 # install Azure CLI
 curl -sSL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
@@ -65,13 +75,6 @@ declare npm_feed_url pat_base64
 npm_feed_url="pkgs.dev.azure.com/VolvoGroup-MASDCL/VCEBusInfoServLayer/_packaging/VCE-MS-PoC/npm"
 pat_base64=$(az keyvault secret show --vault-name kv-vce-devops-agents2 --name AzDevopsPatTokenBase64 --query "value" --output tsv)
 
-# install node lts
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&
-  sudo apt-get install -y nodejs
-
-# install psql client
-sudo apt-get install -y postgresql-client
-
 # setup user wide .npmrc
 npm config --user set "//${npm_feed_url:?}/registry/:username" "VolvoGroup-MASDCL"
 npm config --user set "//${npm_feed_url:?}/registry/:_password" "${pat_base64:?}"
@@ -80,17 +83,3 @@ npm config --user set registry "https://${npm_feed_url:?}/registry"
 
 # install vsu
 npx -y @volvo/vce-service-util@latest --version
-
-# install PowerShell
-sudo apt-get install -y wget apt-transport-https software-properties-common
-
-# shellcheck disable=SC1091
-source /etc/os-release
-wget -q https://packages.microsoft.com/config/ubuntu/"${VERSION_ID:?}"/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install -y powershell
-
-# install Python with pip
-sudo apt-get install -y python3-pip
