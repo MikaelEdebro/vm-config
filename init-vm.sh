9d5a47c688fd54536a24d50d6f4518d9bb4875b9
Mikael Edebro <mikael.edebro@consultant.volvo.com>
2024-05-30 11:21:40 +0200

    refactor: init script

--- init-vm.sh
+++ init-vm.sh
@@ -5,58 +5,64 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 export IS_SELF_HOSTED_AGENT=true
 
-# update apt-get
+# Constants
+AZ_USER="AzDevOps"
+DOCKER_GPG_KEY_URL="https://download.docker.com/linux/ubuntu/gpg"
+DOCKER_APT_SOURCE="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable"
+NPM_FEED_URL="pkgs.dev.azure.com/VolvoGroup-MASDCL/VCEBusInfoServLayer/_packaging/VCE-MS-PoC/npm"
+
+# Log function
+log() {
+  echo "[INFO] $1"
+}
+
+# Update apt-get
+log "Updating apt-get"
 sudo apt-get update
 
-if [[ "$(whoami)" != "AzDevOps" ]]; then
-  # setup AzDevOps user
-  # Create our user account if it does not exist already
-  if ! id AzDevOps &>/dev/null; then
-    sudo apt-get update
-    sudo useradd -m AzDevOps
-    sudo usermod -a -G adm AzDevOps
-    sudo usermod -a -G sudo AzDevOps
+# Creating AzDevOps user if not exists
+if [[ "$(whoami)" != "$AZ_USER" ]]; then
+  log "Setting up $AZ_USER user"
+  if ! id "$AZ_USER" &>/dev/null; then
+    sudo useradd -m "$AZ_USER"
+    sudo usermod -aG adm,sudo "$AZ_USER"
     sudo chmod -R +r /home
     sudo apt-get install -yq acl
-    setfacl -Rdm "u:AzDevOps:rwX" /home
-    setfacl -Rb /home/AzDevOps
-    echo 'AzDevOps ALL=NOPASSWD: ALL' >>/etc/sudoers
+    setfacl -Rdm "u:$AZ_USER:rwX" /home
+    setfacl -Rb /home/"$AZ_USER"
+    echo "$AZ_USER ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers
   fi
 
-  # run this script as AzDevOps
-  su AzDevOps -c "sudo cat ${BASH_SOURCE[0]} | sudo -u AzDevOps tee /home/AzDevOps/devops.sh"
-  chmod +x /home/AzDevOps/devops.sh
-  su - AzDevOps -c /home/AzDevOps/devops.sh
+  # Running script as AzDevOps user
+  su "$AZ_USER" -c "sudo cat ${BASH_SOURCE[0]} | sudo -u $AZ_USER tee /home/$AZ_USER/devops.sh"
+  chmod +x /home/"$AZ_USER"/devops.sh
+  su - "$AZ_USER" -c /home/"$AZ_USER"/devops.sh
   exit 0
 fi
 
-# install dig, jq, and other utils
+# Install essential packages
+log "Installing essential packages"
 sudo apt-get install -yq curl ca-certificates dnsutils jq zip wget unzip postgresql-client python3-pip
 
-# install docker and other prerequisites (https://docs.docker.com/engine/install/ubuntu/)
-# Add Docker's official GPG key:
+# Install Docker
+log "Setting up Docker repository and installing Docker"
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL "$DOCKER_GPG_KEY_URL" -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-# shellcheck disable=SC1091
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "$DOCKER_APT_SOURCE" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -yq docker-ce
-sudo usermod -a -G docker AzDevOps
+sudo usermod -aG docker "$AZ_USER"
 
-# install node lts
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&
-  sudo apt-get install -y nodejs
+# Install Node LTS
+log "Installing Node.js LTS"
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-# install PowerShell
+# Install PowerShell
+log "Installing PowerShell"
 sudo apt-get install -y apt-transport-https software-properties-common
-
 # shellcheck disable=SC1091
 source /etc/os-release
 wget -q https://packages.microsoft.com/config/ubuntu/"${VERSION_ID:?}"/packages-microsoft-prod.deb
@@ -65,21 +71,23 @@ rm packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y powershell
 
-# install Azure CLI
+# Install Azure CLI
+log "Installing Azure CLI"
 curl -sSL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+# Azure CLI Login
+log "Logging into Azure CLI"
 az login --identity --username /subscriptions/d2e4cd6f-ef6e-476a-a6d7-ef1965d9f557/resourcegroups/rg-vce-devops-agents/providers/Microsoft.ManagedIdentity/userAssignedIdentities/sp-devops-agents
 
-declare npm_feed_url pat_base64
-
-npm_feed_url="pkgs.dev.azure.com/VolvoGroup-MASDCL/VCEBusInfoServLayer/_packaging/VCE-MS-PoC/npm"
+# Setup npm configuration
+log "Setting up npm configuration"
+npm_feed_url="$NPM_FEED_URL"
 pat_base64=$(az keyvault secret show --vault-name kv-vce-agents --name AzDevopsPatTokenBase64 --query "value" --output tsv)
-
-# setup user wide .npmrc
 npm config --user set "//${npm_feed_url:?}/registry/:username" "VolvoGroup-MASDCL"
 npm config --user set "//${npm_feed_url:?}/registry/:_password" "${pat_base64:?}"
 npm config --user set "//${npm_feed_url:?}/registry/:email" "npm requires email to be set but doesn't use the value"
 npm config --user set registry "https://${npm_feed_url:?}/registry"
 
-# install vsu
+# Install vsu
+log "Installing vsu"
 npx -y @volvo/vce-service-util@latest --version
